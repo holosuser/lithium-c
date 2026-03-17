0b86647724f3e5f974cb88dd27bbb4238c061a8d
holos <holosnikitatt@gmail.com>
2026-03-17 17:16:37 +0200

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,131 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<libconfig.h>
+#include"colorlib.h"
+const char *repository;
+const char *logotype=R"(
+| (_) |_| |_ (_)_  _ _ __  
+| | |  _| ' \| | || | '  \ 
+|_|_|\__|_||_|_|\_,_|_|_|_|
+)";
+int initializationpm(int argc, char *argv[]){
+    if(argc < 2){
+        printf("use lithium init\n");
+        return 0;
+    }
+    system("mkdir -p /lithium/pkgs/");
+    sleep(1);
+    system("mkdir -p /lithium/pm");
+    return 0;
+}
+int instl(int argc,char *argv[]){
+    char yorn;
+    if(argc < 3){
+        printf("use lithium load <pkg>\n");
+    }
+    printf("Confirm download package(s)? [y/n]: ");
+    scanf(" %c", &yorn);
+    if(yorn == 'y'){
+        for(int i=2; i<argc;i++){
+            char ttyi[256];
+            char chmd[256];
+            char lns[256];
+            printf(GREEN"lithium: installing \x1b[0m%s\n",argv[i]);
+            snprintf(ttyi,sizeof(ttyi),"wget -q --directory=/lithium/pkgs/ %s%s",repository,argv[i]);
+            snprintf(lns,sizeof(lns),"ln -s /lithium/pkgs/%s /bin/%s",argv[i],argv[i]);
+            snprintf(chmd,sizeof(chmd),"chmod +x /lithium/pkgs/%s /bin/%s\n",argv[i],argv[i]);
+            system(ttyi);
+            sleep(1);
+            system(lns);
+            sleep(1);
+            system(chmd);
+        }
+    }
+    if(yorn == 'n'){
+        printf("Aborting.\n");
+    }
+    return 0;
+}
+int rmv(int argc,char *argv[]){
+    char yorn;
+    if(argc < 3){
+        printf("use lithium kill <pkg>\n");
+    }
+    printf("Confirm remove package(s)? [y/n]: ");
+    scanf(" %c", &yorn);
+    if(yorn == 'y'){
+        for(int i=2; i<argc;i++){
+            char ttyr[256];
+            printf(GREEN"lithium: removing \x1b[0m%s\n",argv[i]);
+            snprintf(ttyr,sizeof(ttyr),"rm -rf /lithium/pkgs/%s /bin/%s",argv[i],argv[i]);
+            system(ttyr);
+        }
+    }
+    if(yorn == 'n'){
+        printf("Aborting.\n");
+    }
+    return 0;
+}
+int upd(int argc, char *argv[]){
+    char yorn;
+    printf("Confirm update packages? [y/n] ");
+    scanf(" %c", &yorn);
+    if(yorn == 'y'){
+        printf(GREEN"lithium: updating package manager\n"reset);
+        system("rm /bin/lithium");
+        sleep(1);
+        system("wget -q --directory=/lithium/pm/ https://raw.githubusercontent.com/wholos/LithiumPackages/main/lithium/lithium");
+        sleep(1);
+        system("ln -s /lithium/pm/lithium /bin/lithium");
+        sleep(1);
+        system("chmod +x /lithium/pm/lithium /bin/lithium");
+    }
+    if(yorn == 'n'){
+        printf("Aborting.\n");
+    }
+    return 0;
+}
+int main(int argc,char *argv[]){
+    config_t confg;
+    config_init(&confg);
+    config_read_file(&confg,"/etc/lithium.conf");
+    config_lookup_string(&confg,"repo",&repository);
+    if(argc < 2) {
+        printf("lithium: use --help for documentation if use\n");
+        return 0;
+    }
+    if(strcmp(argv[1],"--help")==0){
+        printf("lithium.\n");
+        printf("version: print version lithium package manager (--version)\n");
+        printf("init: initalization lithium to /mnt\n");
+        printf("load: install package to /bin/ directory\n");
+        printf("kill: remove package from /bin/ directory\n");
+        printf("upd: update packages from /bin/ directory\n");
+        return 0;
+    }
+    if(strcmp(argv[1],"--version")==0){
+        printf(BLUE"LITHIUM"reset);
+        printf(logotype);
+        printf("lithium version - 0.4\n");
+        return 0;
+    }
+    if(strcmp(argv[1],"init")==0){
+        initializationpm(argc,argv);
+        return 0;
+    }
+    if(strcmp(argv[1],"load")==0){
+        instl(argc,argv);
+        return 0;
+    }
+    if(strcmp(argv[1],"kill")==0){
+        rmv(argc,argv);
+        return 0;
+    }
+    if(strcmp(argv[1],"upd")==0){
+        upd(argc,argv);
+        return 0;
+    }
+    printf("lithium: argument %s not found\n",argv[1]);
+}
